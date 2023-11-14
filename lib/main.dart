8d2904413fbcd51db6eed0d7a2030654c70ed26d
ozlemkayyaa <ozlemkayyaa@hotmail.com>
2023-11-14 16:16:42 +0300

    Workshop7

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,9 @@
-//import 'package:expenseapp/models/expense.dart';
+import 'package:expenseapp/models/expense.dart';
 import 'package:expenseapp/pages/expenses_page.dart';
+import 'package:expenseapp/pages/main_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          //centerTitle: true,
-          title: const Text(
-            'ExpenseApp',
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.add_alert),
-            ),
-          ],
-        ),
-        body: const ExpensesPage(),
-      ),
-    ),
-  );
+  runApp(MaterialApp(
+      theme: ThemeData(useMaterial3: true), home: const MainPage()));
 }
